74667435b73ea33577a8803f46466cfcac57a34d
yotsaphonSutweha <komsan_58@hotmail.com>
2020-03-26 18:53:18 +0000

    implements service discovery for python server

--- src/curtain_client.py
+++ src/curtain_client.py
@@ -2,6 +2,39 @@ import grpc
 
 import curtain_pb2
 import curtain_pb2_grpc
+import socket
+from time import sleep
+import sys
+import logging
+from zeroconf import ServiceBrowser, ServiceStateChange, Zeroconf
+
+def on_service_state_change(zeroconf, service_type, name, state_change):
+    
+     if state_change is ServiceStateChange.Added:
+        info = zeroconf.get_service_info(service_type, name)
+        if info:
+            print("Address: %s:%d" % (socket.inet_ntoa(info.address), info.port))
+            print("Weight: %d, priority: %d" % (info.weight, info.priority))
+            print("Server: %s" % (info.server,))
+            if info.properties:
+                print("Properties are:")
+                for key, value in info.properties.items():
+                    print("%s: %s" % (key, value))
+            else:
+                print("No properties")
+        else:
+            print("No info")
+        print('\n')
+
+if __name__ == '__main__':
+    logging.basicConfig(level=logging.DEBUG)
+    if len(sys.argv) > 1:
+        assert sys.argv[1:] == ['--debug']
+        logging.getLogger('zeroconf').setLevel(logging.DEBUG)
+
+    zeroconf = Zeroconf()
+    print("\nBrowsing services, press Ctrl-C to exit...\n")
+    browser = ServiceBrowser(zeroconf, "_http._tcp.local.", handlers=[on_service_state_change])
 
 # open grpc channel 
 channel = grpc.insecure_channel('localhost:8080')
@@ -29,4 +62,5 @@ width_height_adjustment = curtain_pb2.HeightAndWidth(width=1, height=2)
 width_height_response = stub.adjustHeightAndWidth(width_height_adjustment)
 
 print(width_height_response.stringResponseValue)
+
 
